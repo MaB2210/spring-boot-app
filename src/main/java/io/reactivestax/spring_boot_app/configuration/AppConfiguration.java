b5bc3df671c5e9427ceb796f2abf25da27c60cf9
RobinBajaj123 <RobinBajaj@Gmail.com>
2025-01-14 12:50:32 -0500

    all things working for now

--- src/main/java/io/reactivestax/spring_boot_app/configuration/AppConfiguration.java
+++ src/main/java/io/reactivestax/spring_boot_app/configuration/AppConfiguration.java
@@ -1,30 +1,24 @@
 package io.reactivestax.spring_boot_app.configuration;
 
-import javax.sql.DataSource;
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 public class AppConfiguration {
 
-    @Bean
-    public DataSource dataSource() {
-        HikariConfig hikariConfig = new HikariConfig();
+    // @Bean
+    // public DataSource dataSource() {
+    //     HikariConfig hikariConfig = new HikariConfig();
 
-        // Override default HikariCP properties
-        hikariConfig.setJdbcUrl("jdbc:postgresql://localhost:5432/employee_db"); // Database URL
-        hikariConfig.setUsername("postgres"); // Database username
-        hikariConfig.setPassword("postgres22"); // Database password
-        hikariConfig.setMaximumPoolSize(10); // Maximum number of connections in the pool
-        hikariConfig.setMinimumIdle(5); // Minimum number of idle connections
-        hikariConfig.setIdleTimeout(30000); // Idle timeout (in milliseconds)
-        hikariConfig.setConnectionTimeout(20000); // Connection timeout (in milliseconds)
-        // hikariConfig.setDriverClassName("com.mysql.cj.jdbc.Driver"); // JDBC Driver
+    //     // Override default HikariCP properties
+    //     hikariConfig.setJdbcUrl("jdbc:postgresql://localhost:5432/employee_db"); // Database URL
+    //     hikariConfig.setUsername("postgres"); // Database username
+    //     hikariConfig.setPassword("postgres22"); // Database password
+    //     hikariConfig.setMaximumPoolSize(10); // Maximum number of connections in the pool
+    //     hikariConfig.setMinimumIdle(5); // Minimum number of idle connections
+    //     hikariConfig.setIdleTimeout(30000); // Idle timeout (in milliseconds)
+    //     hikariConfig.setConnectionTimeout(20000); // Connection timeout (in milliseconds)
+    //     // hikariConfig.setDriverClassName("com.mysql.cj.jdbc.Driver"); // JDBC Driver
 
-        return new HikariDataSource(hikariConfig);
-    }
+    //     return new HikariDataSource(hikariConfig);
+    // }
 }
--- src/test/java/io/reactivestax/spring_boot_app/service/EmployeeServiceTest.java
+++ src/test/java/io/reactivestax/spring_boot_app/service/EmployeeServiceTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import io.reactivestax.spring_boot_app.domain.Employee;
@@ -17,6 +18,7 @@ import io.reactivestax.spring_boot_app.dto.EmployeeDTO;
 import io.reactivestax.spring_boot_app.repository.EmployeeRepository;
 
 @SpringBootTest // This annotation is used to load the Spring context before the test is run
+@ActiveProfiles("test")
 public class EmployeeServiceTest {
 
     @Autowired
